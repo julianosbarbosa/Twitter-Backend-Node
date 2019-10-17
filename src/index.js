ca495543a31497e1fbbd23bbadfd82e72ca9b8a2
Juliano <julianotrabalho2016@gmail.com>
2019-10-17 12:58:00 -0300

    UPDATE - backend w/ websocket finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 mongoose.connect('mongodb+srv://goweek:goweek123@cluster0-hferg.mongodb.net/test?retryWrites=true&w=majority',{
   useNewUrlParser: true
 })
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
 
+app.use((req, res, next)=>{
+  req.io = io;
+  return next();
+});
+
+app.use(cors())
+app.use(express.json())
 app.use(require('./routes'))
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
   console.log('server started')
 })
 
